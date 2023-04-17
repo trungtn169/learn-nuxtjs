d2f3d5df4b79c1b2c204c9db3dcd61a734c23349
Trung Tran <trung.tran@ursa.vn>
2023-04-17 17:56:07 +0700

    Update code

---
+++ frontend/components/AddTodo.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-col class="add-todo">
+    <v-form ref="formAddTodo" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="title"
+        :rules="titleRules"
+        required
+        label="Add Title"
+      ></v-text-field>
+      <v-btn
+        depressed
+        :disabled="!valid"
+        color="primary"
+        class="mr-4"
+        @click="validate"
+      >
+        Add
+      </v-btn>
+    </v-form>
+  </v-col>
+</template>
+<script>
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      valid: false,
+      title: '',
+      titleRules: [
+        (v) => !!v || 'Title is required',
+        (v) => v.length >= 3 || 'Name must be greater than 03 characters',
+      ],
+    }
+  },
+  methods: {
+    validate() {
+      this.$refs.formAddTodo.validate()
+    },
+  },
+}
+</script>
+<style lang=""></style>
---
+++ frontend/layouts/homepage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <h1 align="center" class="primary py-5">
+      <span class="white--text">TODO APP</span>
+    </h1>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer absolute class="secondary justify-center py-3">
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {}
+  },
+}
+</script>
